fe7e18abe21ad56d3aa2a58d8c18c0006064e47e
Franz Granlund <franz.granlund@alcom.ax>
2017-11-16 19:12:40 +0200

    Test cleanup

--- test/JwtFunctionalTest.java
+++ test/JwtFunctionalTest.java
@@ -19,21 +19,14 @@ public class JwtFunctionalTest extends WithServer {
 
     @Before
     public void before() throws IOException, ExecutionException {
-        try (WSClient ws = play.test.WSTestClient.newClient(this.testServer.port())) {
-            String url = "http://localhost:" + this.testServer.port() + "/";
-            CompletionStage<WSResponse> stage = ws.url(url).get();
-            WSResponse response = stage.toCompletableFuture().get();
-            assertEquals(OK, response.getStatus());
-            token = response.getBody().replace("signed token: ", "");
-        } catch (InterruptedException e) {
-            fail("should not fail");
-        }
-
+        WSResponse response = fetch("/");
+        assertEquals(OK, response.getStatus());
+        token = response.getBody().replace("signed token: ", "");
         assertNotNull(token);
     }
 
     private WSResponse fetch(String url) throws IOException, ExecutionException {
-        try (WSClient ws = play.test.WSTestClient.newClient(this.testServer.port())) {
+        try (WSClient ws = play.test.WSTestClient.newClient(play.api.test.Helpers.testServerPort())) {
             CompletionStage<WSResponse> stage = ws.url(url).get();
             return stage.toCompletableFuture().get();
         } catch (InterruptedException e) {
@@ -43,7 +36,7 @@ public class JwtFunctionalTest extends WithServer {
     }
 
     private WSResponse fetchWithToken(String url, String token) throws IOException, ExecutionException {
-        try (WSClient ws = play.test.WSTestClient.newClient(this.testServer.port())) {
+        try (WSClient ws = play.test.WSTestClient.newClient(play.api.test.Helpers.testServerPort())) {
             CompletionStage<WSResponse> stage = ws.url(url).addHeader("Authorization", "Bearer " + token).get();
             return stage.toCompletableFuture().get();
         } catch (InterruptedException e) {
@@ -54,19 +47,15 @@ public class JwtFunctionalTest extends WithServer {
 
     @Test
     public void testRequiresJwt() throws Exception {
-        String url = "http://localhost:" + this.testServer.port() + "/requires-jwt";
+        String url = "/requires-jwt";
         assertEquals(FORBIDDEN, fetch(url).getStatus());
-
-        url = "http://localhost:" + this.testServer.port() + "/requires-jwt";
         assertEquals(OK, fetchWithToken(url, token).getStatus());
     }
 
     @Test
     public void testRequiresJwtViaFilter() throws Exception {
-        String url = "http://localhost:" + this.testServer.port() + "/requires-jwt-via-filter";
+        String url = "/requires-jwt-via-filter";
         assertEquals(FORBIDDEN, fetch(url).getStatus());
-
-        url = "http://localhost:" + this.testServer.port() + "/requires-jwt-via-filter";
         assertEquals(OK, fetchWithToken(url, token).getStatus());
     }
 }
